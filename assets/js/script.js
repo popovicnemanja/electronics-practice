730ed1a389f24c322d86bb2911242c095473319b
Nemanja Popovic <nemanjaapopovic@gmail.com>
2016-04-26 10:55:38 +0200

    finish practice

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.categories').on('click', function(){
+    	$('nav').slideToggle(800);
+    });
+
+    $('.links h3').on('click', function(){
+    	$('.drop').slideToggle(800);
+
+    });
+
+    $('.links h3').on('click', function(){
+    	$('.drop2').slideToggle(800);
+
+    });
+});
